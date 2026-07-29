54e0f9dd08df774492a845de25c7a975d0a0f6e0
Alex_DS <alexkh2017@gmail.com>
2026-07-29 16:22:10 -0500

    updated app

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import plotly.express as px
 import os
 
 # Kubernetes Service URL
-API_URL = os.getenv("API_URL", "http://localhost:8000/predict")
+API_URL = os.getenv("API_URL", "https://cross-able-willing.ngrok-free.dev -> http://localhost:8000/predict")
 
 st.set_page_config(
     page_title="Fraud Detection",
